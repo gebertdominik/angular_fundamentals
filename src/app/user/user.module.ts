dc51b0acbf9c79243a18ca01274402cc43b8bcf4
gebertdominik <dgebert91@gmail.com>
2020-04-26 16:58:19 +0100

    Create template-based login form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { userRoutes } from './user.routes';
 import {ProfileComponent} from './profile/profile.component';
-
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(userRoutes)
   ]
 })

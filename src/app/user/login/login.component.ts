29f187e7604a61d7ff663f9e902616174a739419
gebertdominik <dgebert91@gmail.com>
2020-04-26 22:46:23 +0100

    Add validation to template-based form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -11,6 +11,7 @@ export class LoginComponent implements OnInit {
 
   userName: string;
   password: string;
+  mouseOverLogin: boolean;
 
   constructor(private authService: AuthService, private router: Router) {
   }
